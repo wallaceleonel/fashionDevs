9836e70cfb977e187fd1b0dc51e793bc0d6b875a
Your Name <wallaceleonel>
2021-10-14 22:32:50 -0300

    adicionando comandos restantes

---
+++ backEndBase/dotnet_projects/bank_app/ClienteApp/Cliente_app.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace bank_app
+{
+    class Cliente_app
+    {
+        public string nome {get; set ;}
+        public  string CPF { get; set;}
+        public  string profissão {get; set;}
+        
+        public Cliente_app( string nome , string CPF ,string profissao)
+        {
+            this.setnome(nome);
+            this.setCPF(CPF);
+            this.setprofissao(profissao);
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+         public void  setnome(string nome)
+        {
+            this.nome = nome;
+        }
+         public string getCPF()
+        {
+            return this.CPF;
+        }
+         public void setCPF(string CPF)
+        {
+            this.CPF = CPF;
+        }
+         public static getprofissao()
+        {
+            return this.profissao;
+        }
+         public void setprofissao(string profissao)
+        {
+            this.profissão = profissao;
+        }
+    }
+}
---
+++ backEndBase/dotnet_projects/bank_app/bank_app/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace bank_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente_app cliente = new Cliente_app("Wallace Leonel","xxx.xxx.xxx-xx","Desenvolvedor");
+            
+            /*
+            metodo alternativo
+            cliente.setNome("Wallace Leonel");
+            cliente.setCPF("xxx.xxx.xxx-xx");
+            cliente.setProfissão("Desenvolvedor");
+            */
+
+            conta_corret_app conta = new conta_corret_app();
+            
+            conta.titular =cliente;
+            conta.agencia = 01;
+            conta.conta = 000012;
+            conta.saldo = 1000000000000.00;
+
+            Console.WriteLine("conta Corrente Criada ");
+            Console.WriteLine("Titular : {0}", conta.titular.nome);
+             Console.WriteLine("Agencia  : {0}", conta.agencia);
+              Console.WriteLine("Conta : {0}", conta.conta);
+        }
+    }
+}
